4ab114b57480705bc2afd53c0a0f13e51f4a1e3a
Noah Lundquist <nalundquist@gmail.com>
2022-08-18 12:37:14 -0700

    bugfix: fix bug flagging numbers as not numbers
    Co-authored-by: Spencer Dennis <sjdennis2113@gmail.com>
    co-authored-by: Helen Lehrer <hlehrer@outlook.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,18 @@
 function cCAnalyzer(input){
 	const numInput = parseInt(input);
+  console.log("numInput= " + typeof numInput);
+  console.log("first= " + typeof numInput);
 	const errorResult = "Error: Invalid input";
-	if (input === NaN || input === undefined || input === null || typeof input === 'string'){
-		return errorResult;
-	} 
+  const numErrorResult = "Error: Second"
+	if ( input === NaN || input === undefined || input === null || input.length > 16 || input.length < 15) {
+    console.log("second= " + typeof numInput);
+    return errorResult;
+  }
+  else if (typeof numInput != "number") {
+    console.log("third= " + typeof numInput);
+    return numErrorResult;
+  } 
 }
+
+
+
